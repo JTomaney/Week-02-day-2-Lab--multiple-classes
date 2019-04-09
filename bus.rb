71a9a71dccb6ecab33c4b53b2f39468b9db22502
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-04-09 15:04:02 +0100

    part B complete

--- bus.rb
+++ bus.rb
@@ -2,14 +2,29 @@ class Bus
   def initialize(route, destination)
     @route = route
     @destination = destination
+    @passengers = []
   end
 
-  attr_reader :route, :destination
+  attr_reader :route, :destination, :passengers
 
   def drive()
     return "Brum brum"
   end
 
+  def count_passengers
+    return @passengers.count
+  end
 
+  def add_passenger(person)
+    @passengers.push(person)
+  end
+
+  def remove_passenger(person)
+    @passengers.delete(person)
+  end
+
+  def remove_all_passengers
+    @passengers = []
+  end
 
 end
--- specs/bus_specs.rb
+++ specs/bus_specs.rb
@@ -6,15 +6,39 @@ require_relative("../person.rb")
 class BusTest < MiniTest::Test
 
   def setup
-    @bus = Bus.new(54, "Glenrothes")
+    @person1 = Person.new("Jon", "27")
+    @person2 = Person.new("James", "33")
+    @bus1 = Bus.new(54, "Glenrothes")
   end
 
   def test_can_get_bus_route
-    assert_equal(54, @bus.route)
+    assert_equal(54, @bus1.route)
   end
 
   def test_drive_returns_string
-    assert_equal("Brum brum", @bus.drive)
+    assert_equal("Brum brum", @bus1.drive)
+  end
+
+  def test_can_get_bus_passengers
+    assert_equal(0, @bus1.count_passengers)
+  end
+
+  def test_can_add_passenger_to_bus
+    @bus1.add_passenger(@person1)
+    assert_equal(1, @bus1.count_passengers)
+  end
+
+  def test_can_remove_passenger_from_bus
+    @bus1.add_passenger(@person1)
+    @bus1.remove_passenger(@person1)
+    assert_equal(0, @bus1.count_passengers)
+  end
+
+  def test_can_remove_all_passengers
+    @bus1.add_passenger(@person2)
+    @bus1.add_passenger(@person1)
+    @bus1.remove_all_passengers
+    assert_equal(0, @bus1.count_passengers)
   end
 
 end
